9ffb374e8fe6e7fcd428d65f7858f244de18e682
Deyber Manuel Parra Romero <deyber.parra@gmail.com>
2022-10-16 12:15:18 -0500

    se crea el splash y logo de la app

--- app/src/main/java/com/deyber/beersommelier/ui/MainActivity.kt
+++ app/src/main/java/com/deyber/beersommelier/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.deyber.beersommelier.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.deyber.beersommelier.R
 import com.deyber.beersommelier.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_BeerSommelier)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
---
+++ app/src/main/java/com/deyber/beersommelier/ui/fragments/OnboardingFragment.kt
@@ -0,0 +1,37 @@
+package com.deyber.beersommelier.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.deyber.beersommelier.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+    private lateinit var binding:FragmentOnboardingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentOnboardingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+}
